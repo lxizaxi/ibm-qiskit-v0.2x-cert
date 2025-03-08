7ff1c95394141c3dbefc0671bce8faa02b601e26
shota <email.example>
2025-03-08 13:41:42 +0900

    env: gitignoreにpycache追加 / main.pyを空にした

--- main.py
+++ main.py
@@ -6,29 +6,7 @@ app = marimo.App(width="medium")
 
 @app.cell
 def _():
-    from qiskit import QuantumCircuit, Aer, execute
-    from math import sqrt
-    qc = QuantumCircuit(2)
-
-    # Insert fragment here
-    qc.h(0)
-    qc.cx(0,1)
-
-    simulator = Aer.get_backend('statevector_simulator')
-    result = execute(qc, simulator).result()
-    statevector = result.get_statevector()
-    print(statevector)
-
-    return (
-        Aer,
-        QuantumCircuit,
-        execute,
-        qc,
-        result,
-        simulator,
-        sqrt,
-        statevector,
-    )
+    return
 
 
 if __name__ == "__main__":
