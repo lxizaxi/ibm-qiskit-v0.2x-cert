8705a5733d554aaf735a022e84d5e4bab06a5bbc
Mona Lisa <email.example>
2025-03-08 13:03:58 +0900

    env: marimoの動確を兼ねて、サンプル問題8のコードを実行

--- main.py
+++ main.py
@@ -1,6 +1,35 @@
-def main():
-    print("Hello from ibm-qiskit-v2-0x-cert!")
+import marimo
+
+__generated_with = "0.11.17"
+app = marimo.App(width="medium")
+
+
+@app.cell
+def _():
+    from qiskit import QuantumCircuit, Aer, execute
+    from math import sqrt
+    qc = QuantumCircuit(2)
+
+    # Insert fragment here
+    qc.h(0)
+    qc.cx(0,1)
+
+    simulator = Aer.get_backend('statevector_simulator')
+    result = execute(qc, simulator).result()
+    statevector = result.get_statevector()
+    print(statevector)
+
+    return (
+        Aer,
+        QuantumCircuit,
+        execute,
+        qc,
+        result,
+        simulator,
+        sqrt,
+        statevector,
+    )
 
 
 if __name__ == "__main__":
-    main()
+    app.run()
